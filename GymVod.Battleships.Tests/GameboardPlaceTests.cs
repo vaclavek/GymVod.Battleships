317527a1b65a007f75fcd4f2bf112ef92d4ab83a
Ondřej Václavek <vaclavek@jinsoft.cz>
2020-02-25 23:22:37 +0100

    Add tests

---
+++ GymVod.Battleships.Tests/GameboardPlaceTests.cs
@@ -0,0 +1,214 @@
+using GymVod.Battleships.Common;
+using GymVod.Battleships.Services.GameServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymVod.Battleships.Tests
+{
+    [TestClass]
+    public class GameboardPlaceTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBePlacedOutsideOfBoardInRight()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(20,2), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBePlacedOutsideOfBoardInBottom()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(3,20), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBeTouchBoardInTop()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Submarine, new Position(3,0), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBeTouchBoardInRight()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Submarine, new Position(9,3), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBeTouchBoardInBottom()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Submarine, new Position(3,9), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotBeTouchBoardInLeft()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Submarine, new Position(0,5), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotOverlap1()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Down),
+                new ShipPosition(ShipType.Carrier, new Position(1,1), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotOverlap2()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Down),
+                new ShipPosition(ShipType.Cruiser, new Position(1,4), Orientation.Up)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotOverlap3()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Right),
+                new ShipPosition(ShipType.Cruiser, new Position(4,1), Orientation.Left)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotTouchRight()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Right),
+                new ShipPosition(ShipType.Cruiser, new Position(5,1), Orientation.Left)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GameOverException))]
+        public void GameboardTest_ShipsCannotTouchDown()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Down),
+                new ShipPosition(ShipType.Cruiser, new Position(1,5), Orientation.Up)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+
+            // assert (exception is thrown)
+        }
+
+    }
+}
---
+++ GymVod.Battleships.Tests/ShootTests.cs
@@ -0,0 +1,56 @@
+﻿using GymVod.Battleships.Common;
+using GymVod.Battleships.Services.GameServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymVod.Battleships.Tests
+{
+    [TestClass]
+    public class ShootTests
+    {
+
+        [TestMethod]
+        public void GameboardTest_DestroyerSunk()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+            var result1 = gb1.Shoot(new Position(1, 1));
+            var result2 = gb1.Shoot(new Position(1, 2));
+
+            // assert
+            Assert.IsTrue(result1.Hit);
+            Assert.IsFalse(result1.ShipSunken);
+
+            Assert.IsTrue(result2.Hit);
+            Assert.IsTrue(result2.ShipSunken);
+        }
+
+        [TestMethod]
+        public void GameboardTest_UserHasLost()
+        {
+            // arrange
+            ShipPosition[] s1 =
+            {
+                new ShipPosition(ShipType.Destroyer, new Position(1,1), Orientation.Down)
+            };
+
+            Gameboard gb1 = new Gameboard(10, 10);
+
+            // act
+            gb1.PlaceShips(s1);
+            gb1.Shoot(new Position(1, 1));
+            gb1.Shoot(new Position(1, 2));
+
+            // assert
+            Assert.IsTrue(gb1.UserHasLost());
+        }
+
+    }
+}
